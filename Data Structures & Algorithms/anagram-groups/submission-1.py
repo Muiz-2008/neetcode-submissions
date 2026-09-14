2d6022921ef02dbd0d0d7e69a64757815cbcadc3
Muiz-2008 <afolabiabdulmuiz5@gmail.com>
2026-09-14 10:23:04 +0100

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # create a dictionary
+        # go through the list
+        # create a positions map
+        # find the order of each letter 
+        # use as key for hashmap
+        # return the dictionary as values
+        dictionary_words = defaultdict(list) 
+
+        for each_word in strs:
+            map_letters = [0] * 26
+
+            for each_letter in each_word:
+                map_letters[ord(each_letter) - ord("a")] += 1
+            dictionary_words[tuple(map_letters)].append(each_word)
+
+        return list(dictionary_words.values())
+
